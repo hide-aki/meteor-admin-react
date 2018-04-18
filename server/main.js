03f453b2c6454a519b76cfe86c8504010fde3718
Daniel Pinna <danielpinna@outlook.com.br>
2018-04-18 01:06:22 -0300

    fix send mail

--- server/main.js
+++ server/main.js
@@ -2,4 +2,17 @@ import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
   // code to run on server at startup
+
+  Accounts.emailTemplates.from = 'Gestao Igrejas <contato@gestaoigrejas.com.br>';
+  
+  //Verificação da conta
+  Accounts.emailTemplates.verifyEmail = {
+    subject() {
+       return "Confirme seu email";
+    },
+    text(user, url) {
+       return `Olá ${user.profile.name}! Para confirmar seu email, basta clicar no link : ${url}`;
+    }
+  };
+
 });
