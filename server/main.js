41ea70b69211b693a1cce20ea273dfd411d60d68
Daniel Pinna <danielpinna@outlook.com.br>
2018-04-18 01:23:04 -0300

    changed template emails from accounts

--- server/main.js
+++ server/main.js
@@ -3,15 +3,36 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // code to run on server at startup
 
-  Accounts.emailTemplates.from = 'Gestao Igrejas <contato@gestaoigrejas.com.br>';
-  
+  Accounts.emailTemplates.siteName = 'Sistema';
+  Accounts.emailTemplates.from = 'Sistema <danielpinna2@gmail.com>';
+
+  //Criação da conta
+  Accounts.emailTemplates.enrollAccount = {
+    subject(user) {
+       return `${user.profile.name}, bem-vindo ao Sistema!`
+    },
+    text(user, url) {
+       return `Olá ${user.profile.name}! Para ativar sua conta, basta copiar e colar na barra de endereços este link : ${url}`;
+    }
+  };
+
   //Verificação da conta
   Accounts.emailTemplates.verifyEmail = {
     subject() {
-       return "Confirme seu email";
+       return "Validação de email";
     },
     text(user, url) {
-       return `Olá ${user.profile.name}! Para confirmar seu email, basta clicar no link : ${url}`;
+       return `Olá ${user.profile.name}! Para confirmar seu email, basta copiar e colar na barra de endereços este link : ${url}`;
+    }
+  };
+
+  //Reset de senha
+  Accounts.emailTemplates.resetPassword = {
+    subject() {
+       return "Sistema - Password Reset";
+    },
+    text(user, url) {
+       return `Olá ${user.profile.name}! Para resetar sua senha, basta copiar e colar na barra de endereços este link : ${url}`;
     }
   };
 
